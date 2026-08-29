18a3ac55472fdfb29ad044da20b13e2e291470f6
Sp20078 <saiyampatil88@gmail.com>
2026-08-29 17:55:03 +0530

    Added carousel feature

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const DeepTraceApp());
+  final prefs = await SharedPreferences.getInstance();
+  final onboardingComplete = prefs.getBool('onboarding_complete') ?? false;
+  runApp(DeepTraceApp(onboardingComplete: onboardingComplete));
 }
